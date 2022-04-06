7aac470185c58867b457511a8a3a06f49a625a67
ictus <ictus@ictusui-MacBookPro.local>
2022-04-06 11:06:19 +0900

    1-4 alias

---
+++ 1-Types/1-4-alias.ts
@@ -0,0 +1,25 @@
+{
+    //Type Alias
+    type Text = string
+    const name: Text = 'soo'
+    const address: Text = 'korea'
+    type Num = number
+    type Student = {
+        name: string
+        age: number
+    }
+    const student: Student = {
+        name: 'soo',
+        age: 24
+    }
+
+    // String Literal Types
+    type Name = 'name'
+    let sooName: Name
+    sooName = 'name'
+    type Json = 'json'
+    const json: Json = 'json'
+
+    type Bool = true
+    const isCat: Bool = false
+}
