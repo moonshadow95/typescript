1c1d25f0b36090c2e67c3db7443914ff3ac9f5cc
ictus <ictus@ictusui-MacBookPro.local>
2022-04-06 10:39:39 +0900

    JS -> TS

--- 1-Types/1-2-function.ts
+++ 1-Types/1-2-function.ts
@@ -4,10 +4,27 @@
         return num1 + numb2
     }
 
-    // TypeScript
+    // TypeScript ✨
     function add(num1: number, numb2: number): number {
         return num1 + numb2
     }
 
+    // JavaScript 💩
+    function jsFetchNum(id) {
+        // code...
+        // code...
+        return new Promise((resolve, reject) => {
+            resolve(100)
+        })
+    }
+
+    // TypeScript ✨
+    function fetchNum(id: string): Promise<number> {
+        // code...
+        // code...
+        return new Promise((resolve, reject) => {
+            resolve(100)
+        })
+    }
 
 }
