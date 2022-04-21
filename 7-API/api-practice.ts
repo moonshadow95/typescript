18cbb81df7a0e1c7631c2b354a9f99360682abdc
ictus <ictus@ictusui-MacBookPro.local>
2022-04-21 17:54:27 +0900

    API

--- 7-API/api-practice.ts
+++ 7-API/api-practice.ts
@@ -1,6 +1,5 @@
 {
-    Array;
-
+    // every() 사용해보기
     class Animal {
     }
 
@@ -12,11 +11,13 @@
         isDog: boolean = true
     }
 
-    const animals: Animal[] = [new Cat(), new Cat(), new Dog()]
+    const animals: Animal[] = [new Cat(), new Dog(), new Dog()]
 
+    // 인자로 받은 animal 이 Cat 타입인 경우 반환한다.
     function isCat(animal: Animal): animal is Cat {
         return (animal as Cat).isCat !== undefined
     }
 
+    // callback 함수를 전달하여 Cat 인지 아닌지 타입을 확인한다.
     animals.every<Cat>(isCat)
 }
