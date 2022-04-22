16b02268cce17d1eb116b079e70f2a5907d32fd2
ictus <ictus@ictusui-MacBookPro.local>
2022-04-22 11:29:42 +0900

    Error Handling - 2. Handling

--- 8-exception/8-2-handling.ts
+++ 8-exception/8-2-handling.ts
@@ -8,6 +8,7 @@ class UserService {
     constructor(private client: NetworkClient) {
     }
 
+    // 여기서 에러를 캐치하면 의미 있는 처리를 할 수 없음
     login() {
         this.client.tryConnect()
         // login...
@@ -18,7 +19,7 @@ class App {
     constructor(private userService: UserService) {
     }
 
-    // 의미있는 처리를 할 수 있는 곳에서 에러를 캐치해야 한다.
+    // 의미 있는 처리를 할 수 있는 곳에서 에러를 캐치해야 한다.
     run() {
         try {
             this.userService.login()
