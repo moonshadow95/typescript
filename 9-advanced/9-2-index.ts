269513c529c5ef2bdb01b14d84a4fc0af13185db
ictus <ictus@ictusui-MacBookPro.local>
2022-04-22 16:38:57 +0900

    Index type

---
+++ 9-advanced/9-2-index.ts
@@ -0,0 +1,32 @@
+{
+    const obj = {
+        name: 'soo'
+    }
+    obj.name // 'soo'
+    obj[name] // 'soo'
+
+    type Animal = {
+        name: string
+        age: number
+        gender: 'male' | 'female'
+    }
+
+    type Name = Animal['name'] // string
+
+    const text: Name = 'hello'
+
+    type Gender = Animal['gender'] // 'male'|'female'
+
+    type Keys = keyof Animal // 'name'|'age'|'gender'
+
+    const key: Keys = 'gender'
+
+    type Person = {
+        name: string
+        gender: Animal['gender']
+    }
+    const Person: Person = {
+        name: 'soo',
+        gender: 'female'
+    }
+}
