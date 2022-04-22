9637dfe90190c0c88eb489fcbcbb898147ca0b9d
ictus <ictus@ictusui-MacBookPro.local>
2022-04-22 14:23:02 +0900

    Type Alias, Interface - 1

---
+++ 9-advanced/9-1-interface.ts
@@ -0,0 +1,50 @@
+type PositionType = {
+    x: number
+    y: number
+}
+
+interface PositionInterface {
+    x: number
+    y: number
+}
+
+// 인터페이스는 같은 이름으로 한번 더 정의하면 두개가 합쳐진다. x,y,z 모두 가짐
+// interface PositionInterface {
+//     z: number
+// }
+
+// Object
+const obj_1: PositionType = {
+    x: 1,
+    y: 1
+}
+const obj_2: PositionInterface = {
+    x: 1,
+    y: 1
+}
+
+// Class
+class Pos1 implements PositionType {
+    x: number
+    y: number
+}
+
+class Pos2 implements PositionInterface {
+    x: number
+    y: number
+}
+
+// Extends
+interface ZPositionInterface extends PositionInterface {
+    z: number
+}
+
+type ZPositionType = PositionType & { z: number }
+
+// Type aliases can use computed properties!
+type Person = {
+    name: string
+    age: number
+}
+type Name = Person['name'] // string
+type NumberType = number
